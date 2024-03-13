14126bd294d6c2dfce7d9a6e9d74ac4e16d7b56a
sergeyblck <41168589+sergeyblck@users.noreply.github.com>
2024-03-13 21:45:58 +0000

    two more

---
+++ screens/ProfileScreen.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View } from "react-native";
+import Profile from "../components/Profile";
+
+const ProfileScreen = () => {
+  return (
+    <View>
+      <Profile username="JohnDoe" />
+    </View>
+  );
+};
+
+export default ProfileScreen;
