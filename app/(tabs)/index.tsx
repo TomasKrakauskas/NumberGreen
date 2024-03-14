a5d36e37352e9bd280466e23531977a601a195e6
Stephen Adesina <bauerstevenson@gmail.com>
2024-03-14 03:59:56 +0200

    checkiing auth before allowing access

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,34 @@
-import { StyleSheet } from "react-native";
+import React, { useEffect } from 'react';
+import { StyleSheet } from 'react-native';
+import { View, Text } from '@/components/Themed';
+import EditScreenInfo from '@/components/EditScreenInfo';
+import {onAuthStateChanged } from 'firebase/auth';
+import { useNavigation } from '@react-navigation/native';
+import { NavigationProp } from '@react-navigation/native';
+import {auth} from '@/firebaseConfig'
 
-import EditScreenInfo from "@/components/EditScreenInfo";
-import { Text, View } from "@/components/Themed";
+type RootStackParamList = {
+  LoginScreen: undefined; 
+  
+};
 
 export default function TabOneScreen() {
+  const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        navigation.navigate('LoginScreen'); 
+    }});
+  
+
+        return unsubscribe;
+  }, [navigation]); 
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
   );
@@ -20,16 +37,16 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   title: {
     fontSize: 20,
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: "80%",
+    width: '80%',
   },
 });
