5615b9bc25c871da897b5cd93808c788e07b4706
Stephen Adesina <bauerstevenson@gmail.com>
2024-04-06 17:58:43 +0300

    goals page

--- app/(tabs)/goals-screen.tsx
+++ app/(tabs)/goals-screen.tsx
@@ -3,52 +3,65 @@ import { StyleSheet, Text, View } from "react-native";
 import Value from "@/components/Value";
 import RingProgress from "@/components/RingProgress";
 import { useEffect, useState } from "react";
-import { auth } from "@/firebaseConfig";
-import  firestore from "@react-native-firebase/firestore";
+import { auth, db } from "@/firebaseConfig";
+import { doc, onSnapshot } from "firebase/firestore";
 import { Image } from "react-native";
 
+interface Badge {
+  badge_link: string;
+  badge_name: string;
+  has_badge: boolean;
+}
+
+interface UserData {
+  badges: Badge[];
+  daily_km: string;
+  streak: string;
+  user_id: string;
+  weekly_km: string;
+}
+
 export default function App() {
-  const [userData, setUserData] = useState(null);
+  const [userData, setUserData] = useState<UserData | null>(null);
 
   useEffect(() => {
     const user = auth.currentUser;
     if (user) {
-      const subscriber = firestore()
-        .collection("goals")
-        .doc(user.uid)
-        .onSnapshot(
-          (documentSnapshot) => {
-            if (documentSnapshot.exists) {
-              console.log("User data:", documentSnapshot.data());
-              //@ts-ignore
-              setUserData(documentSnapshot.data());
-            } else {
-              console.log("Document does not exist");
-            }
-          },
-          (error) => {
-            console.log(error);
+      const unsubscribe = onSnapshot(
+        doc(db, "goals", user.uid),
+        (documentSnapshot) => {
+          if (documentSnapshot.exists()) {
+            console.log("User data:", documentSnapshot.data());
+            setUserData(documentSnapshot.data() as UserData);
+          } else {
+            console.log("Document does not exist");
           }
-        );
-
-      return () => subscriber();
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+      return unsubscribe;
     } else {
       console.log("No user logged in");
+      return;
     }
   }, []);
 
   return (
     <View style={styles.container}>
+      {/** Need to get how much they've walked today and add calculate the value below for the ring */}
+      <RingProgress progress={0.8} />
+
       <View style={styles.values}>
-        <RingProgress progress={0.8} />
+        <Value label="Streak" value={userData?.streak || ""} />
+        <Value label="Daily Goals" value={userData?.daily_km || ""} />
+        <Value label="Weekly Goals" value={userData?.weekly_km || ""} />
+      </View>
 
-        <Value label="Streak" value={(userData as any)?.streak} />
-        <Value label="Daily Goals" value={(userData as any)?.daily_km} />
-        <Value label="Weekly Goals" value="x km walked/ y total" />
-{ /* @ts-ignore */ }
-
-        {(userData as any)?.badges &&
-          (userData as any).badges.map((badge: any, index: number) => (
+      <View style={styles.badgesContainer}>
+        {userData?.badges &&
+          userData.badges.map((badge, index) => (
             <View
               key={index}
               style={{ alignItems: "center", marginBottom: 20 }}
@@ -58,7 +71,7 @@ export default function App() {
                 style={{ width: 100, height: 100, marginBottom: 5 }}
                 resizeMode="contain"
               />
-              <Text>{badge.badge_name}</Text>
+              <Text style={{ color: "#FFFFFF" }}>{badge.badge_name}</Text>
             </View>
           ))}
       </View>
@@ -72,12 +85,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "black",
+    alignItems: "center",
     justifyContent: "center",
     padding: 12,
   },
   values: {
+    marginTop: 50,
     flexDirection: "row",
-    gap: 25,
-    flexWrap: "wrap",
+    justifyContent: "space-around",
+    width: "100%",
+  },
+  badgeStyle: {
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  badgesContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    width: "100%",
+    marginTop: 50,
   },
 });
