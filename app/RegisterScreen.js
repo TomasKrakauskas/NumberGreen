acec11770f1d71b87b03503c1a08820d8576c3e1
Stephen Adesina <bauerstevenson@gmail.com>
2024-04-07 09:56:07 +0300

    removing name field

--- app/RegisterScreen.js
+++ app/RegisterScreen.js
@@ -1,69 +1,56 @@
-import React, { useState } from 'react'
-import { View, StyleSheet, TouchableOpacity } from 'react-native'
-import { Text } from 'react-native-paper'
-import Background from '@/components/Background'
-import Logo from '@/components/Logo'
-import Header from '@/components/Header'
-import Button from '@/components/Button'
-import TextInput from '@/components/TextInput'
-import { theme } from '@/core/theme'
-import {createUserFirestoreDocs} from '@/helpers/createUserDocs'
-import { emailValidator } from '@/helpers/emailValidator'
-import { passwordValidator } from '@/helpers/passwordValidator'
-import { nameValidator } from '@/helpers/nameValidator'
+import React, { useState } from "react";
+import { View, StyleSheet, TouchableOpacity } from "react-native";
+import { Text } from "react-native-paper";
+import Background from "@/components/Background";
+import Logo from "@/components/Logo";
+import Header from "@/components/Header";
+import Button from "@/components/Button";
+import TextInput from "@/components/TextInput";
+import { theme } from "@/core/theme";
+import createUserFirestoreDocs from "@/helpers/createUserDocs";
+import { emailValidator } from "@/helpers/emailValidator";
+import { passwordValidator } from "@/helpers/passwordValidator";
 import { auth } from "@/firebaseConfig";
 import { useNavigation } from "@react-navigation/native";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 
 export default function RegisterScreen() {
-    const navigation = useNavigation();
-  const [name, setName] = useState({ value: '', error: '' })
-  const [email, setEmail] = useState({ value: '', error: '' })
-  const [password, setPassword] = useState({ value: '', error: '' })
+  const navigation = useNavigation();
+  const [email, setEmail] = useState({ value: "", error: "" });
+  const [password, setPassword] = useState({ value: "", error: "" });
 
   const onSignUpPressed = () => {
-    const nameError = nameValidator(name.value)
-    const emailError = emailValidator(email.value)
-    const passwordError = passwordValidator(password.value)
-    if (emailError || passwordError || nameError) {
-      setName({ ...name, error: nameError })
-      setEmail({ ...email, error: emailError })
-      setPassword({ ...password, error: passwordError })
-      return
+    const emailError = emailValidator(email.value);
+    const passwordError = passwordValidator(password.value);
+    if (emailError || passwordError) {
+      setEmail({ ...email, error: emailError });
+      setPassword({ ...password, error: passwordError });
+      return;
     }
-     createUserWithEmailAndPassword(auth, email.value, password.value)
-    .then((userCredential) => {
-      
-      const user = userCredential.user;
-      createUserFirestoreDocs(user.uid);
-    
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-     
-    });
-    navigation.navigate("(tabs)");
-  }
+    createUserWithEmailAndPassword(auth, email.value, password.value).then(
+      (userCredential) => {
+        const user = userCredential.user;
+        createUserFirestoreDocs(user)
+          .then(() => {
+            navigation.navigate("(tabs)");
+          })
+          .catch((error) => {
+            console.error("Error creating user documents:", error);
+          });
+      }
+    );
+  };
 
   return (
     <Background>
-      {/* <BackButton goBack={navigation.goBack} /> */}
       <Logo />
       <Header>Create Account</Header>
-      <TextInput
-        label="Name"
-        returnKeyType="next"
-        value={name.value}
-        onChangeText={(text) => setName({ value: text, error: '' })}
-        error={!!name.error}
-        errorText={name.error}
-      />
+
       <TextInput
         label="Email"
         returnKeyType="next"
         value={email.value}
-        onChangeText={(text) => setEmail({ value: text, error: '' })}
+        onChangeText={(text) => setEmail({ value: text, error: "" })}
         error={!!email.error}
         errorText={email.error}
         autoCapitalize="none"
@@ -75,7 +62,7 @@ export default function RegisterScreen() {
         label="Password"
         returnKeyType="done"
         value={password.value}
-        onChangeText={(text) => setPassword({ value: text, error: '' })}
+        onChangeText={(text) => setPassword({ value: text, error: "" })}
         error={!!password.error}
         errorText={password.error}
         secureTextEntry
@@ -89,21 +76,21 @@ export default function RegisterScreen() {
       </Button>
       <View style={styles.row}>
         <Text>Already have an account? </Text>
-        <TouchableOpacity onPress={() => navigation.replace('LoginScreen')}>
+        <TouchableOpacity onPress={() => navigation.replace("LoginScreen")}>
           <Text style={styles.link}>Login</Text>
         </TouchableOpacity>
       </View>
     </Background>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   row: {
-    flexDirection: 'row',
+    flexDirection: "row",
     marginTop: 4,
   },
   link: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
     color: theme.colors.primary,
   },
-})
+});
