9fa2601c6e5f06fb12959eda0297a917a657ea83
TomasKrakauskas <Tomas.krakauskas1999@gmail.com>
2024-04-17 10:22:29 +0000

    feat: Track slice

--- helpers/util.ts
+++ helpers/util.ts
@@ -4,3 +4,9 @@ export function getUnixTimeStamp(date: Date | number): number {
 
   return Math.floor(date.getTime() / 1000);
 }
+export const setAll = (state: any, properties: any) => {
+  const props = Object.keys(properties);
+  props.forEach((key) => {
+    state[key] = properties[key];
+  });
+};
