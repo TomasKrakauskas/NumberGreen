9b5c4cd582b836a1fc1a7a0806501d626d74d403
TomasKrakauskas <Tomas.krakauskas1999@gmail.com>
2024-05-24 08:16:09 +0000

    fix: adjust merge issues

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -80,28 +80,30 @@ function RootLayoutNav() {
   return (
     <PaperProvider>
       <ThemeProvider value={DefaultTheme}>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="modal" options={{ presentation: "modal" }} />
-          <Stack.Screen name="LoginScreen" options={{ headerShown: false }} />
-          <Stack.Screen
-            name="RegisterScreen"
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="AchievementsScreen"
-            options={{ headerShown: false }}
-          />
+        <Provider store={store}>
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+            <Stack.Screen name="LoginScreen" options={{ headerShown: false }} />
+            <Stack.Screen
+              name="RegisterScreen"
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="AchievementsScreen"
+              options={{ headerShown: false }}
+            />
 
-          <Stack.Screen name="ResetPasswordScreen" />
-          <Stack.Screen
-            name="path"
-            options={{
-              title: "Path",
-            }}
-            initialParams={{ trackId: -1 }}
-          />
-        </Stack>
+            <Stack.Screen name="ResetPasswordScreen" />
+            <Stack.Screen
+              name="path"
+              options={{
+                title: "Path",
+              }}
+              initialParams={{ trackId: undefined }}
+            />
+          </Stack>
+        </Provider>
       </ThemeProvider>
     </PaperProvider>
   );
