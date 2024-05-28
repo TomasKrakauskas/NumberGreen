0050d3692c6abd01bc308470fff5d89719eaa10c
Stephen Adesina <bauerstevenson@gmail.com>
2024-05-28 20:08:20 +0300

    adding to the badges model

--- helpers/createUserDocs.ts
+++ helpers/createUserDocs.ts
@@ -18,18 +18,27 @@ export default async function createUserFirestoreDocs(userId: string) {
         "https://cdn2.iconfinder.com/data/icons/gamification-badges-1/300/streak_7d1-1024.png",
       badge_name: "7 days",
       has_badge: false,
+      current_progress: 0,
+      overall_progress: 100,
+      description: "Complete 7 days of running streak",
     },
     {
       badge_link:
         "https://img.freepik.com/premium-vector/1-month-money-back-guarantee-gold-badges_669907-29.jpg",
       badge_name: "1 month",
       has_badge: false,
+      current_progress: 0,
+      overall_progress: 100,
+      description: "Complete 1 month of running streak",
     },
     {
       badge_link:
         "https://audaxmedals.southportcc.co.uk/wp-content/uploads/2017/01/Badge16-BP100.jpg",
       badge_name: "100 km",
       has_badge: false,
+      current_progress: 0,
+      overall_progress: 100,
+      description: "Complete 100 km of running",
     },
   ];
 
--- interfaces/badge.ts
+++ interfaces/badge.ts
@@ -2,6 +2,9 @@ export interface Badge {
   badge_link: string;
   badge_name: string;
   has_badge: boolean;
+  description: string;
+  current_progress: number;
+  overall_progress: number;
 }
 
 
